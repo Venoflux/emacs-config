b5e9660d0834c3f4eb87066fff1999a8cf2e409b
Venoflux <erolsarpk@gmail.com>
2023-11-30 13:10:20 +0300

    Update all

---
+++ lisp/core-emacs-config.el
@@ -0,0 +1,80 @@
+(use-package
+ emacs
+ :ensure nil
+ :elpaca nil
+ :demand nil
+ :hook (after-init . (lambda () (setq gc-cons-threshold (* 2 1000 1000))))
+ :custom
+
+ ;; Should use:
+ (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+ ;; at least once per installation or while changing this list
+ (treesit-language-source-alist
+  '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+    (heex "https://github.com/phoenixframework/tree-sitter-heex")
+    (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
+    (clojure "https://github.com/sogaiu/tree-sitter-clojure")))
+
+ (major-mode-remap-alist
+  '((bash-mode . bash-ts-mode)
+    (elixir-mode . elixir-ts-mode)
+    (clojure-mode . clojure-ts-mode)))
+ 
+ :config
+ ;; Startup screen and warning light
+ (setq inhibit-startup-screen t
+       visible-bell nil)
+
+ ;; Turnoff unneeded UI elements
+ (menu-bar-mode -1)
+ (tool-bar-mode -1)
+ (scroll-bar-mode -1)
+
+ ;; Line Numbers
+ ;; Enable line numbers globally
+ (global-display-line-numbers-mode t)
+ (dolist (mode '(org-mode-hook
+                 term-mode-hook
+                 eshell-mode-hook))
+   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+ ;; Highlight line mode
+                                        ;(global-hl-line-mode t)
+
+ (blink-cursor-mode t) ;; Blinking cursor mode
+ (recentf-mode 1) ;; Remember recently edited files
+
+ ;; Save what you enter into minibuffer prompts
+ (setq history-length 25)
+ (savehist-mode 1)
+
+ ;; Remember and restore the last cursor location of opened files
+ (save-place-mode 1)
+ ;; Don't pop up UI dialogs when prompting
+ (setq use-dialog-box nil)
+ ;; Revert buffers when the underlying file has changed
+ (global-auto-revert-mode 1)
+ ;; Revert Dired and other buffers
+ (setq global-auto-revert-non-file-buffers t)
+
+ ;; Slow Rendering Fonts
+ (setq inhibit-compacting-font-caches t)
+
+ ;; Prevent Extraneous Tabs
+ (setq-default indent-tabs-mode nil)
+
+ ;; Font
+ (add-to-list 'default-frame-alist
+              '(font . "Cascadia Code-14"))
+
+ (global-prettify-symbols-mode)
+
+ :bind
+ (("C-+" . text-scale-increase)
+  ("C--" . text-scale-decrease)
+  ("<f5>" . treemacs)
+  ("<f6>" . flymake-show-buffer-diagnostics)
+  ("<f7>" . eat)
+  ("C-o" . other-window)
+  ("s-b" . switch-to-buffer)
+  ("C-q" . kill-buffer)))
---
+++ lisp/external-packages-config.el
@@ -0,0 +1,124 @@
+(use-package
+ exec-path-from-shell
+ :demand t
+ :config (exec-path-from-shell-copy-env "SSH_AUTH_SOCK") (exec-path-from-shell-initialize))
+
+(elpaca-wait)
+
+(use-package treemacs :demand t)
+
+(use-package eat
+  :config (add-hook 'eat-mode-hook (lambda () (display-line-numbers-mode -1)))
+  :bind (("C-o" . other-window) ("<f5>" . treemacs)))
+
+(elpaca-wait)
+
+(use-package
+ helpful
+ :demand t
+ :bind
+ ("C-h f" . helpful-callable)
+ ("C-h v" . helpful-variable)
+ ("C-h k" . helpful-key)
+ ("C-h F" . helpful-function)
+ ("C-h C" . helpful-command))
+(elpaca-wait)
+
+(use-package company :demand t :config (global-company-mode))
+(elpaca-wait)
+
+(use-package lsp-mode
+  :demand t
+  :commands lsp
+  :ensure t
+  :diminish lsp-mode
+  :hook
+  (elixir-ts-mode . lsp))
+(elpaca-wait)
+
+(use-package lsp-ui :demand t :commands lsp-ui-mode)
+(elpaca-wait)
+
+(use-package lsp-treemacs :demand t :commands lsp-treemacs-errors-list)
+(elpaca-wait)
+
+(use-package which-key :demand t :config (which-key-mode))
+(elpaca-wait)
+
+(use-package smartparens :demand t :hook (prog-mode . smartparens-global-mode))
+
+(use-package rainbow-mode :demand t :diminish rainbow-mode)
+(elpaca-wait)
+(use-package rainbow-delimiters :demand t :hook (prog-mode . rainbow-delimiters-mode))
+(elpaca-wait)
+
+;; Catppuccin Theme
+(use-package catppuccin-theme :demand t)
+(elpaca-wait)
+
+(load-theme 'catppuccin :no-confirm)
+
+(elpaca-wait)
+
+;; Ligatures
+(use-package ligature
+  :demand t
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
+
+(elpaca-wait)
+
+(use-package yasnippet :demand t :diminish yas-minor-mode :config (yas-global-mode 1))
+(elpaca-wait)
+(use-package yasnippet-snippets :demand t :after (yasnippet))
+(elpaca-wait)
+(use-package doom-snippets :after yasnippet :elpaca (:host github :repo "doomemacs/snippets"))
+(elpaca-wait)
+
+(use-package
+ elixir-ts-mode
+ :demand t
+ :hook (elixir-ts-mode . lsp))
+(elpaca-wait)
+
+(use-package cider :demand t)
+(elpaca-wait)
+(use-package
+  clojure-ts-mode
+  :demand t
+  :hook
+  (clojure-ts-mode . lsp)
+  (cider-preferred-build-tool . lein))
+(elpaca-wait)
+
+(use-package all-the-icons :demand t :if (display-graphic-p))
+(elpaca-wait)
+
+(use-package
+ doom-modeline
+ :demand t
+ :after all-the-icons
+ :init (doom-modeline-mode)
+ :custom (doom-modeline-height 35))
+(elpaca-wait)
